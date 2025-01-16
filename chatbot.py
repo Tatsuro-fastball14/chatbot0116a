25381e96e5042f6a688eb91f8460a2d0ce1e98eb
Tatsuro-fastball14 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2025-01-16 20:42:14 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -17,7 +17,7 @@ import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
 
-with open('/Users/tatsuro/Desktop/venv/bin/config.yaml') as file:
+with open('/config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 # Pre-hashing all plain text passwords once
